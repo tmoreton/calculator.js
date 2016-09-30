b4cfb2a656d500908242638b910d232bc1cdcc54
Tim Moreton <tmoreton89@gmail.com>
2016-09-30 10:04:37 -0400

    refactored calculator functionality

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
-var result = "0";
+var result = [];
 var operators = ["+", "-", "/", "x"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-var buttons = document.querySelectorAll( '.calc-btn' );
 
-for(var i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", function(){
-		// Add function here
-		console.log("keydown")
-	});
-};
+// var buttons = document.querySelectorAll( '.calc-btn' );
+// for(var i = 0; i < buttons.length; i++) {
+// 	buttons[i].addEventListener("click", function(){
+// 		// Add function here
+// 		console.log("keydown")
+// 	});
+// };
+
 
 function calculator(el) {
+	el = String(el)
     if (numbers.indexOf(el) != -1){
-    	result += el
-    	document.getElementById('result').innerHTML = result
+ 		if (result.length < 1){
+ 			result.push(el)
+ 			document.getElementById('result').innerHTML = el
+ 		} else if (result.length == 1) {
+ 			result[0] = result[0]+el
+ 			document.getElementById('result').innerHTML = result[0]
+ 		} else if (result.length == 2){
+ 			result.push(el)
+ 			document.getElementById('result').innerHTML = el
+ 		} else if (result.length == 3){
+ 			result[2] = result[2]+el
+ 			document.getElementById('result').innerHTML = result[2]
+ 		}
     } else if (operators.indexOf(el) != -1){
-    	result += el
-    	document.getElementById('result').innerHTML = result
+ 		if (result.length == 1){
+ 			result.push(el)
+ 		} else if (result.length > 1 && operators.indexOf(result[1]) != -1) {
+ 			var newArray = String(eval(result.join("")))
+ 			document.getElementById('result').innerHTML = newArray
+ 			result = [newArray]
+ 		}
 	}
 }
 
 function zero() {
-	result = "0"
-	document.getElementById('result').innerHTML = result
+	result = []
+	document.getElementById('result').innerHTML = "0"
 }
 
 function total(){
-	document.getElementById('result').innerHTML = eval(result)
+	var newArray = String(eval(result.join("")))
+	document.getElementById('result').innerHTML = newArray
+	result = [newArray]
+}
+
+function exp(){
+	
 }
 
 document.addEventListener('keydown', function(event) {
